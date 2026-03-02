374302017bd51585c6c1dc090b9324104fbf712d
fulgence <officialfulgence@gmail.com>
2026-03-02 19:39:12 +0200

    Updated Railway DB

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,10 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'project' );
-
-/** Database username */
+define( 'DB_NAME', 'railway' );
 define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_PASSWORD', 'KwUFvdEcmxSBaEkgnlQtRupiCyIUZMOq' );
+define( 'DB_HOST', 'ballast.proxy.rlwy.net:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
